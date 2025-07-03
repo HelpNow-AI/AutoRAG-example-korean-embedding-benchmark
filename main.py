12455a0dc122b54b2c5a954b55b9200c1b8241d8
최한준(Choe, Hanjoon) <hanjoon.choe@bespinglobal.com>
2025-07-03 15:32:37 +0900

    add : 모델 변경

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def main(config, qa_data_path, corpus_data_path, project_dir):
     load_dotenv()
 
     # autorag.embedding_models = {}
-    autorag.embedding_models['mxbai-embed-xsmall-v1'] = autorag.LazyInit(HuggingFaceEmbedding, model_name="mixedbread-ai/mxbai-embed-xsmall-v1")
+    autorag.embedding_models['mxbai-embed-large-v1'] = autorag.LazyInit(HuggingFaceEmbedding, model_name="mixedbread-ai/mxbai-embed-large-v1")
     # autorag.embedding_models['bge-m3'] = autorag.LazyInit(HuggingFaceEmbedding, model_name="BAAI/bge-m3")
     # autorag.embedding_models['qwen3-embedding-0.6b'] = autorag.LazyInit(HuggingFaceEmbedding, model_name="Qwen/Qwen3-Embedding-0.6B")
     # autorag.embedding_models['inf-retriever-v1-1.5b'] = autorag.LazyInit(HuggingFaceEmbedding, model_name="infly/inf-retriever-v1-1.5b")
